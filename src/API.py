a68bfd8e4a4d6ba86a48b0713e26fa4d42103123
Antos99999 <147400067+Antos99999@users.noreply.github.com>
2025-03-10 17:30:39 +0100

    Update API.py

--- src/API.py
+++ src/API.py
@@ -7,32 +7,32 @@ import json
 def AccountsV1(tagLine,gameName):
     Nick = urllib.parse.quote(gameName)
     try:
-        accountInfo = requests.get('https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'+Nick+'/'+tagLine+'?api_key=RGAPI-eaedea2d-4a12-4005-97e5-4466a2397777')
+        accountInfo = requests.get('https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'+Nick+'/'+tagLine+'?api_key='PUT_UR_KEY_HERE'')
     except:
         time.sleep(121)
-        accountInfo = requests.get('https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'+Nick+'/'+tagLine + '?api_key=' + key)
+        accountInfo = requests.get('https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'+Nick+'/'+tagLine + '?api_key='PUT_UR_KEY_HERE'')
     return accountInfo.json()
 
 def MatchV5(puuid):
     try:
-        metch = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/'+puuid+'/ids?start=0&count=20&api_key=RGAPI-eaedea2d-4a12-4005-97e5-4466a2397777')
+        metch = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/'+puuid+'/ids?start=0&count=20&api_key='PUT_UR_KEY_HERE'')
     except:
         time.sleep(121)
-        metch = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/'+puuid+'/ids?start=0&count=20&api_key=RGAPI-eaedea2d-4a12-4005-97e5-4466a2397777')
+        metch = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/'+puuid+'/ids?start=0&count=20&api_key='PUT_UR_KEY_HERE'')
     return metch.json()
 
 def MatchInfoV5(matchId):
     try:
-        matchInfo = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/'+matchId+'?api_key=RGAPI-eaedea2d-4a12-4005-97e5-4466a2397777')
+        matchInfo = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/'+matchId+'?api_key='PUT_UR_KEY_HERE'')
     except:
         time.sleep(121)
-        matchInfo = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/'+matchId+'?api_key=RGAPI-eaedea2d-4a12-4005-97e5-4466a2397777')
+        matchInfo = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/'+matchId+'?api_key='PUT_UR_KEY_HERE'')
     return matchInfo.json()
 
 def MatchTimeLine(matchId):
     try:
-        matchTimeLine = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/'+matchId+'/timeline?api_key=RGAPI-eaedea2d-4a12-4005-97e5-4466a2397777')
+        matchTimeLine = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/'+matchId+'/timeline?api_key='PUT_UR_KEY_HERE'')
     except:
         time.sleep(121)
-        matchTimeLine = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/' + matchId + '/timeline?api_key=' + key)
+        matchTimeLine = requests.get('https://europe.api.riotgames.com/lol/match/v5/matches/' + matchId + '/timeline?api_key='PUT_UR_KEY_HERE'')
     return matchTimeLine.json()
